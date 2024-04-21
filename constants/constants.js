8e31cdec28430e247a789d6c6558359e17d1b09f
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-04-21 14:25:17 +0200

    Change API calls with constants

--- constants/constants.js
+++ constants/constants.js
@@ -66,3 +66,88 @@ export const SOCIAL_LINKS = {
     LINKEDIN: "https://www.linkedin.com/in/barkin-karadeniz/",
     TWITTER: "https://twitter.com/KaradenizBarkin",
 }
+
+export const background = [
+    {
+        eduCards: [
+            {
+                id: 0,
+                title: "Technical University of Berlin",
+                degree: "BS, Computer Science",
+                detail: "Bachelor's Degree in Computer Science from TU Berlin.",
+                year: "2019-2023",
+            },
+            {
+                id: 1,
+                title: "German School of Istanbul",
+                degree: "High School",
+                detail: "Turkish and German High School Diploma from German School of Istanbul.",
+                year: "2014-2019",
+            },
+        ],
+    },
+    {
+        expCards: [
+            {
+                id: 1,
+                role: "Data Science Intern",
+                title: "DAI-Labor der TU Berlin",
+                url: "https://dai-labor.de/",
+                desc: "The content of the work I did for DAI-Labor is confidential, due to a non-disclosure agreement",
+                year: "10/2022 - 03/2023",
+                location: "Berlin, Germany",
+            },
+            {
+                id: 2,
+                role: "Junior Fullstack Software Engineer",
+                title: "Coyotiv GmbH",
+                url: "https://coyotiv.com/",
+                desc: "Started as an Intern and promoted to Junior Fullstack Software Engineer after few months",
+                year: "08/2021 - 10/2022",
+                location: "Berlin, Germany",
+            },
+            {
+                id: 3,
+                role: "IT-Intern",
+                title: "Karden İnşaat A.Ş.",
+                url: "https://www.kardeninsaat.com.tr/",
+                desc: "Worked as an Intern in the IT Department of Karden İnşaat A.Ş. for 3 months",
+                year: "07/2020 - 09/2020",
+                location: "Istanbul, Turkiye",
+            },
+        ],
+    },
+]
+
+export const expertise = [
+    {
+        id: 0,
+        title: "Full Stack Development",
+        desc: "My main expertise and profesion is Full Stack Development. I have been working as a Full Stack Developer for 1,5 years. I have experience in developing web applications using MERN stack. I have also worked on various projects using MERN stack, Vue.js and SQL. I have a strong understanding of RESTful APIs and have experience in building and consuming them.",
+    },
+    {
+        id: 1,
+        title: "Data Science",
+        desc: "As a computer science graduate, I am also somewhat experienced in data science. I have worked on projects involving data analysis, data visualization, and machine learning. I have experience in using Python libraries such as Pandas, NumPy, Matplotlib, Scikit-learn and TensorFlow. I have also worked on projects involving natural language processing. I am always looking to learn more about data science and machine learning.",
+    },
+    {
+        id: 2,
+        title: "Renewable Energy",
+        desc: "Right now, I also provide consultancy services in the field of renewable energy, specialized in solar energy particularly. I have experience in conducting feasibility studies, designing renewable energy systems, and conducting energy audits. My experience is focused mostly in Türkiye and Germany.",
+    },
+    {
+        id: 3,
+        title: "Economy and Finance",
+        desc: "I am proud to say that I have a strong understanding of economics and finance. Even though I am not an economist, I am financially literate since many years now. I have experience in investing in the stock market, cryptocurrencies and real estates. I hope to find myself a good spot in the FinTech industry in the future using both my engineering and finance knowledge.",
+    },
+    {
+        id: 4,
+        title: "Entrepreneurship",
+        desc: "A life long dream of mine is to become an entrepreneur. I have been working on various projects and ideas for years now. I have some knowledge in business development, marketing, and sales and I hope to transform this knowledge into experiences and success in the future.",
+    },
+    {
+        id: 5,
+        title: "European Youth Parliament",
+        desc: "I have been a member of the European Youth Parliament since 2017. I have attended various sessions and events in different countries. I have experience in public speaking, debating, and writing. I am also a member of the Alumni Association of the European Youth Parliament.",
+    },
+]
