4a38b7bb6234108dc8c399317ccd815bd77435c6
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-04-21 11:49:35 +0200

    Experimenting with GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
-module.exports = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
-    output: "export",
-    basePath: "/PersonalWebsite",
+// module.exports = {
+//     eslint: {
+//         // Warning: This allows production builds to successfully complete even if
+//         // your project has ESLint errors.
+//         ignoreDuringBuilds: true,
+//     },
+//     // output: "export",
+//     // basePath: "/PersonalWebsite",
+//     // distDir: "out",
+// }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     distDir: "out",
+    output: "export",
+    reactStrictMode: true,
 }
+
+module.exports = nextConfig
