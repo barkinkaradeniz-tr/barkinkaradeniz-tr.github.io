740ffda89567aad835ae75c251982a3c383c7b4b
Barkin Karadeniz <barkinkaradeniz2000@gmail.com>
2024-04-25 20:52:56 +0200

    Fix footer positioning

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,73 +10,81 @@ const Contact = () => {
 
     return (
         <BannerLayout>
-            <div className=" px-4 py-2">
-                <div className="my-6 text-Snow flex flex-col gap-y-5">
-                    <h1 className="text-lg font-bold">Contact Information</h1>
-                    <div className="flex flex-col md:flex-row items-center gap-5 text-xs">
-                        <div className="card_stylings w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
-                            <div className="flex justify-between items-center">
-                                <span className="md:text-base">Country:</span>
-                                <span className="text-LightGray md:text-sm">
-                                    Germany
-                                </span>
+            <div className="flex flex-col min-h-screen px-4 py-2">
+                <div className="flex-grow">
+                    <div className="my-6 text-Snow flex flex-col gap-y-5">
+                        <h1 className="text-lg font-bold">
+                            Contact Information
+                        </h1>
+                        <div className="flex flex-col md:flex-row items-center gap-5 text-xs">
+                            <div className="card_stylings w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+                                <div className="flex justify-between items-center">
+                                    <span className="md:text-base">
+                                        Country:
+                                    </span>
+                                    <span className="text-LightGray md:text-sm">
+                                        Germany
+                                    </span>
+                                </div>
+                                <div className="flex justify-between items-center">
+                                    <span className="md:text-base">City:</span>
+                                    <span className="text-LightGray md:text-sm">
+                                        Berlin
+                                    </span>
+                                </div>
                             </div>
-                            <div className="flex justify-between items-center">
-                                <span className="md:text-base">City:</span>
-                                <span className="text-LightGray md:text-sm">
-                                    Berlin
-                                </span>
-                            </div>
-                        </div>
-                        <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
-                            <div className="flex justify-between items-center">
-                                <span className="md:text-base">Email:</span>
-                                <span className="text-LightGray text-sm">
-                                    barkinkaradeniz@icloud.com
-                                </span>
-                            </div>
-                            <div className="flex justify-between items-center">
-                                <span className="md:text-base">Linkedin:</span>
-                                <span className="text-LightGray text-sm">
-                                    barkin-karadeniz
-                                </span>
+                            <div className="card_stylings rounded-xl w-full md:w-1/2 p-5 md:p-6 lg:p-8 flex flex-col gap-y-4">
+                                <div className="flex justify-between items-center">
+                                    <span className="md:text-base">Email:</span>
+                                    <span className="text-LightGray text-sm">
+                                        barkinkaradeniz@icloud.com
+                                    </span>
+                                </div>
+                                <div className="flex justify-between items-center">
+                                    <span className="md:text-base">
+                                        Linkedin:
+                                    </span>
+                                    <span className="text-LightGray text-sm">
+                                        barkin-karadeniz
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="h-16 w-full card_stylings text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items-center justify-center text-Snow">
-                    <a
-                        className="hover:scale-125 ease-in-out duration-700"
-                        href="mailto:barkinkaradeniz@icloud.com"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <HiMail />
-                    </a>
-                    <a
-                        className="hover:scale-125 ease-in-out duration-700"
-                        href={SOCIAL_LINKS.GITHUB}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <FaGithub />
-                    </a>
-                    <a
-                        className="hover:scale-125 ease-in-out duration-700"
-                        href={SOCIAL_LINKS.LINKEDIN}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <FaLinkedin />
-                    </a>
-                    <a
-                        className="hover:scale-125 ease-in-out duration-700"
-                        href={SOCIAL_LINKS.TWITTER}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <FaTwitter />
-                    </a>
+                    <div className="h-16 w-full card_stylings text-xl sm:text-3xl flex gap-x-8 sm:gap-x-16 items-center justify-center text-Snow">
+                        <a
+                            className="hover:scale-125 ease-in-out duration-700"
+                            href="mailto:barkinkaradeniz@icloud.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <HiMail />
+                        </a>
+                        <a
+                            className="hover:scale-125 ease-in-out duration-700"
+                            href={SOCIAL_LINKS.GITHUB}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaGithub />
+                        </a>
+                        <a
+                            className="hover:scale-125 ease-in-out duration-700"
+                            href={SOCIAL_LINKS.LINKEDIN}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaLinkedin />
+                        </a>
+                        <a
+                            className="hover:scale-125 ease-in-out duration-700"
+                            href={SOCIAL_LINKS.TWITTER}
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <FaTwitter />
+                        </a>
+                    </div>
                 </div>
             </div>
             <Footer />
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import Banner from "../components/HomeComponents/Banner"
 import MyExpertise from "../components/HomeComponents/Expertise/MyExpertise"
 import Recommendations from "../components/HomeComponents/Recommendations/Recommendations"
 import ClientReviews from "../components/HomeComponents/ClientReviews/ClientReviews"
+import BannerLayout from "../components/Common/BannerLayout"
 
 const home = () => {
     return (
-        <div className="Home-Page -z-10">
-            <Banner />
-            <MyExpertise />
-            {/* <Recommendations /> */}
-            {/* <ClientReviews /> */}
-            <Footer />
+        <div className="Home-Page z-10">
+            <BannerLayout>
+                <Banner />
+                <MyExpertise />
+                {/* <Recommendations /> */}
+                {/* <ClientReviews /> */}
+                <Footer />
+            </BannerLayout>
         </div>
     )
 }
